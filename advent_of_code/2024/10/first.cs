b890b58d74c8d040e7e87b41ab11fc92b97a71b1
Yue Jiao <cleamoon@hotmail.com>
2024-12-11 23:20:53 +0100

    Upload solution(s)

---
+++ advent_of_code/2024/10/first.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "input.txt";
+
+        string[] lines = File.ReadAllLines(filePath);
+
+        int width = lines[0].Length;
+        int height = lines.Length;
+        int[,] map = new int[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                map[x, y] = lines[y][x] - '0';
+            }
+        }
+
+        int ans = 0;
+
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (map[x, y] == 0)
+                {
+                    HashSet<(int, int)> found = new HashSet<(int, int)>();
+                    Queue<(int, int, int)> q = new Queue<(int, int, int)>(new[] { (x, y, 0) });
+
+                    while (q.Count > 0)
+                    {
+                        (int, int, int) pos = q.Dequeue();
+                        int xx = pos.Item1;
+                        int yy = pos.Item2;
+                        int curr = pos.Item3;
+
+                        if (xx < 0 || xx >= width || yy < 0 || yy >= height || curr > 9)
+                        {
+                            continue;
+                        }
+
+                        if (map[xx, yy] == curr)
+                        {
+                            if (curr == 9)
+                            {
+                                if (found.Contains((xx, yy)))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    found.Add((xx, yy));
+                                    ans += 1;
+                                    continue;
+                                }
+                            }
+
+                            q.Enqueue((xx + 1, yy, curr + 1));
+                            q.Enqueue((xx - 1, yy, curr + 1));
+                            q.Enqueue((xx, yy + 1, curr + 1));
+                            q.Enqueue((xx, yy - 1, curr + 1));
+                        }
+                    }
+
+                }
+            }
+        }
+
+        Console.WriteLine(ans);
+    }
+
+}
