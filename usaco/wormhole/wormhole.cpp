a5f8a2691bc9fa6e326cabaf99ef71ae8bbede6f
Yue Jiao <cleamoon@hotmail.com>
2026-02-28 16:43:21 +0100

    Upload solution(s)

---
+++ usaco/wormhole/wormhole.cpp
@@ -0,0 +1,17 @@
+"""
+ID: yj1
+LANG: C++
+TASK: test
+"""
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  ofstream fout("wormhole.out");
+  ifstream fin("wormhole.in");
+  
+  return 0;
+}
